bcd1d64b3a624bc03e1c48a883401157ec5bc1be
양동진 <dong3831@hanmail.net>
2025-01-30 14:09:18 +0900

    solved leetcode9

---
+++ leetcode9.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        length = len(x) // 2
+
+        for i in range(length):
+            if x[i] != x[-i - 1]:
+                return False
+            
+        return True
