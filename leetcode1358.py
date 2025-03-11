a12a9a7a04542403bd282697444090e14ae81c28
양동진 <dong3831@hanmail.net>
2025-03-11 11:10:45 +0900

    add another answer

--- leetcode1358.py
+++ leetcode1358.py
@@ -27,4 +27,24 @@ s = Solution()
 print(s.numberOfSubstrings(s = "abcabc"))
 
 
+
+## 다른 사람 정답이라는데 너무 어렵다..
+
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        # Track last position of a, b, c
+        last_pos = [-1] * 3
+        total = 0
+
+        for pos in range(len(s)):
+            # Update last position of current character
+            last_pos[ord(s[pos]) - ord("a")] = pos
+
+            # Add count of valid substrings ending at current position
+            # If any character is missing, min will be -1
+            # Else min gives leftmost required character position
+            total += 1 + min(last_pos)
+
+        return total
+
         
